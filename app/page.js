2d3b66980dca257ebd623f188d5007ce404fbe55
AlgonzaDJun <algonsadewangga@gmail.com>
2025-03-07 09:17:46 +0900

    add changes env

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-md bg-cyan-800 text-white p-4 rounded-lg mx-auto mt-10">
-      ini home
+    <div className="w-full max-w-md  text-white p-4 rounded-lg mx-auto mt-10">
+      Please access
+      <Link>
+      link
+      </Link>
     </div>
   );
 }
