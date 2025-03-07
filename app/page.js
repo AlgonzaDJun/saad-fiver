c9bf8e2f1055f8f780f8fbffe3dd0d707180a3ed
AlgonzaDJun <algonsadewangga@gmail.com>
2025-03-07 18:20:31 +0900

    home

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-md  text-white p-4 rounded-lg mx-auto mt-10">
+    <div className="w-full max-w-md bg-white text-black p-4 rounded-lg mx-auto mt-10">
       Please access the following pages:
       <Link href="/admin" className="block underline">Admin Page</Link>
     </div>
